7108059778057757b62ac6d5a6eee007c61cda1a
Oliver Abreu <oliverabreu@Olivers-Mac-mini.local>
2021-06-17 14:22:15 -0400

    creates some initial pages

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,12 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import MediaQuery from 'react-responsive'
 
+import LandingPage from './landing'
+// import React, { useState, useEffect } from 'react';
+
+
 export default function Home() {
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,52 +17,15 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
       </Head>
-      <div>
-        <MediaQuery minDeviceWidth={1024} and query="(orientation: landscape)">
-          <h1>Driving Simulator Host Helper Tool 1</h1>
-          <style jsx>{`
-            * {
-              background: #4082eb;
-              height: 100vh;
-              width: 100vw;
-              color: #fff;
-              display: flex;
-              justify-content: center;
-              align-items: center;
-              overflow: hidden;
-              font-family: "Work Sans", sans-serif;
-              font-weight: 400;
-              text-align: center;
-            }
-            h1 {
-              font-size: 1.75rem;
-            }
-          `}</style>
+        <MediaQuery 
+          minDeviceWidth={1024} and query="(orientation: landscape)" 
+          className={styles.landingPage}
+        >
+          <LandingPage />
         </MediaQuery>
         <MediaQuery query="(max-device-width: 1023px)">
-          <div>
-            <h2>Your screen size is too small to use this web app</h2>
-          </div>
-          <style jsx>{`
-            * {
-              background: #cc0000;
-              height: 100vh;
-              width: 100vw;
-              color: #fff;
-              display: flex;
-              justify-content: center;
-              align-items: center;
-              overflow: hidden;
-              font-family: "Work Sans", sans-serif;
-              font-weight: 400;
-              text-align: center;
-            }
-            h2 {
-              font-size: 1.75rem;
-            }
-          `}</style>
+            <h2>Your screen size is too small to use this application</h2>
         </MediaQuery>
-      </div>
     </div>
   )
 }
